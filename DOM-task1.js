970f132a924f6292fa808089737d05f5863e7f37
BhushanBhale27 <bhushan27official@gmail.com>
2023-03-22 22:47:44 +0530

    DOM-task1

---
+++ DOM-task1.js
@@ -0,0 +1,9 @@
+//title :- black border..
+const header = document.getElementById("main-header");
+header.style.borderBottom = "4px solid black"
+
+//ADD ITEMS change to bold and font color to green
+
+const addItems = document.querySelector(".title");
+addItems.style.fontWeight = "bold";
+addItems.style.color = "green";
