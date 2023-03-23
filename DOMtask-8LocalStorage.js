3116fd542f7c3377dbf4510ccd5d2c5ba9ba1f4a
BhushanBhale27 <bhushan27official@gmail.com>
2023-03-23 23:01:37 +0530

    DOMtask-9StoredatainRightFormat

--- DOMtask-8LocalStorage.js
+++ DOMtask-8LocalStorage.js
@@ -2,15 +2,25 @@ function signup(event) {
     event.preventDefault();
 
     const name = document.getElementById("na").value;
-    localStorage.setItem("userName" , name);
+    // localStorage.setItem("userName" , name);
     const email = document.getElementById("em").value;
-    localStorage.setItem("userEmail" , email);
+    // localStorage.setItem("userEmail" , email);
     const phone = document.getElementById("ph").value;
-    localStorage.setItem("userPhone" , phone);
+    // localStorage.setItem("userPhone" , phone);
     const date = document.getElementById("da").value;
-    localStorage.setItem("userDate" , date);
+    // localStorage.setItem("userDate" , date);
     const time = document.getElementById("ti").value;
-    localStorage.setItem("userTime" , time);
+    // localStorage.setItem("userTime" , time);
+
+    const obj = {
+        name,
+        email,
+        phone,
+        date,
+        time
+    }
+    localStorage.setItem("userDetails", JSON.stringify(obj));
+
 
     if (phone.length !== 10) {
 alert("Please enter a 10-digit phone number");
@@ -18,4 +28,3 @@ return;
 }
 }
 
-console.log(localStorage.getItem(userName));
